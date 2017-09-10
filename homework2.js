875327f880a92c6fb54efd174ae953367fb4615b
nvozni <narina232@gmail.com>
2017-09-10 17:24:03 +0400

    Added homework2.js javascript file with basic math operations

---
+++ homework2.js
@@ -0,0 +1,9 @@
+console.log("Basic mathematical operations:");
+var x = 8, y = 15;
+console.log("The sum of x and y is " + (x + y));
+console.log("The multiple of x and y is " + (x * y));
+var z = 150, i = 45;
+console.log("The subtraction of z and i is " + (z - i));
+console.log("The division of z and i is " + (z / i));
+
+
